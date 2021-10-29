d321774fb72589acda21555d22af09ae08460ccf
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-10-29 16:33:18 +0000

    code review

--- script.js
+++ script.js
@@ -138,11 +138,7 @@ let fullscreen = false;
 
 // Toggle fullscreen
 function toggleFullscreen(){
-    if (!fullscreen){
-        openFullscreen(player);
-    } else {
-        closeFullscreen();
-    }
+    !fullscreen ?  openFullscreen(player) : closeFullscreen();
     fullscreen = !fullscreen;
 }
 
