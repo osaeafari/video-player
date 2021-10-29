295f25bc015dec3d52740aa56cbedf3738599339
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-10-29 15:58:43 +0000

    added functionality to fullscreen

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //variables
+const player = document.querySelector('.player');
 const video= document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
@@ -109,8 +110,41 @@ function changeSpeed(){
 
 
 // ----------------------------- Fullscreen ------------------------------- //
+/* View in fullscreen */
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen');
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen');
+  }
 
+let fullscreen = false;
 
+// Toggle fullscreen
+function toggleFullscreen(){
+    if (!fullscreen){
+        openFullscreen(player);
+    } else {
+        closeFullscreen();
+    }
+    fullscreen = !fullscreen;
+}
 
 // ---------------------------- Event Listeners --------------------------- //
 playBtn.addEventListener('click', togglePlay);
@@ -121,3 +155,4 @@ progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
 speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
